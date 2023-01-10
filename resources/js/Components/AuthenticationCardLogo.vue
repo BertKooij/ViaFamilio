f310b8a8fe2fcde38f93c16e460ea4e7f7593f58
Bert Kooij <bert.kooij@beko.design>
2023-01-10 21:24:18 +0100

    Jetstream setup, tailwind config - Fixes #2

---
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import ApplicationLogo from '@/Components/ApplicationLogo.vue';</script>
+
+<template>
+    <Link :href="route('home')">
+        <application-logo class="max-w-100 h-auto p-6" />
+    </Link>
+</template>
